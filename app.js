1ab9313e35ed4bb672c4703744e598d989672c56
Shiva Venkatesh <shiva@bangthetable.com>
2017-04-27 20:53:27 +0530

    module: add fetchWeather module

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var fetchWeather = require('fetchWeather')
 var express = require('express')
 var app = express()
 var path = require('path');
@@ -7,5 +8,8 @@ app.listen(8000, function () {
     app.get('/', function (req, res) {
       res.sendFile(path.join(__dirname + '/views/index.html'));
       // res.render('index', { title: 'Hey', message: 'Hello there!' })
+    })
+    app.get('/forecasts', function(req, res) {
+      res.json(fetchWeather.fetchForecast())
     })
 })
--- controllers/fetchWeather.js
+++ controllers/fetchWeather.js
@@ -2,21 +2,42 @@ var Promise = require('bluebird')
 var app = express()
 
 var request = Promise.promisify(require("request"));
+var rp = require('request-promise');
 
 let baseURL = 'http://dataservice.accuweather.com'
 let location_code = 204108
 
 function fetchForecast() {
+  let store
   let url = baseURL + '/forecasts/v1/hourly/1hour/' + location_code
   let config_properties = { apikey: 'DcLAlS5lGA5mYPJvn4hoGZQN3aGS7DdE',
                             language: 'en-us',
                             details: true, 
                             metric: true }
-  request(baseURL, qs:config_properties)
-    .then(function(response) {
-      app.get('/forecasts', function(req, res) {
-        res.json(response)
-      })
+  // request(url, qs:config_properties)
+  //   .then(function(response) {
+  //     app.get('/forecasts', function(req, res) {
+  //       res.json(response)
+  //     })
+  //  let a = app.get('/forecasts', function(req, res) {
+  //       res.json(response)
+  //     })
+  //   })
+    //
+var options = {
+    uri: url,
+    qs: config_properties,
+    json: true // Automatically parses the JSON string in the response
+};
+
+rp(options)
+    .then(response, function (response) {
+      return response
+    })
+    .catch(function (err) {
+        // API call failed...
+        console.log('API call failed')
     })
 }
+
 
